bf38733c04ca8d62282a0d571e8f08e50b09d783
Raosk <sampreeth20@Hotmail.com>
2016-01-05 08:58:50 -0500

    Website Updates
    Some css changes, javascript additions

--- javascript.js
+++ javascript.js
@@ -34,11 +34,30 @@ fadeinimg('liylembed2');
 //SAME AS THE FUNCTION ABOVE, BUT DOES THE ONCLICK EVENT THROUGH JQUERY INSTEAD OF HTML, UTILIZING THE HTML ID TAG
 $('#liylgrid').click(function(){
   $('#liylembed').fadeIn(750);
+
   document.getElementById('reelembed').style.display = 'none';
+  document.getElementById('waveembed').style.display = 'none';
+  document.getElementById('alifeinprisonembed').style.display = 'none';
+  document.getElementById('stayforloveembed').style.display = 'none';
+  document.getElementById('alreadystartedembed').style.display = 'none';
+
+  document.getElementById('liyltext').style.fontWeight = 'bold';
+  document.getElementById('reeltext').style.fontWeight = 'lighter';
+  document.getElementById('wavetext').style.fontWeight = 'lighter';
+  document.getElementById('alifeinprisontext').style.fontWeight = 'lighter';
+  document.getElementById('stayforlovetext').style.fontWeight = 'lighter';
+  document.getElementById('alreadystartedtext').style.fontWeight = 'lighter';
+
 });
 
-//WE WANT AN ALL-PURPOSE JQUERY COMMAND THAT TAKES IN THE CLICKED ELEMENT AND FADES IN THE CORRESPONDING EMBED
+
 $('#reelgrid').click(function(){
   $('#reelembed').fadeIn(750);
   document.getElementById('liylembed').style.display = 'none';
+});
+
+//I WANT AN ALL-PURPOSE JQUERY COMMAND THAT TAKES IN THE CLICKED ELEMENT AND FADES IN THE CORRESPONDING EMBED
+$('#____grid').click(function(){ //when you click on a ____grid link
+  $('#____embed').fadeIn(750); //you fade in ____embed
+  document.getElementById('____embed').style.display = 'none'; //all other ____embed not equal to the clicked ____grid disappear
 });
