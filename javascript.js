b26674066340def96a513e835b0f7ea2edc5d81f
Raosk <sampreeth20@Hotmail.com>
2016-01-05 08:10:49 -0500

    Website Update
    Added working JQuery fades for embeds

--- javascript.js
+++ javascript.js
@@ -1,23 +1,19 @@
-function showImage(imageId) {
-document.getElementById(imageId).style.display = 'block';
+function showImage(elementId) {
+document.getElementById(elementId).style.display = 'block';
 }
 function hideImage(imageId) {
 document.getElementById(imageId).style.display = 'none';
 }
 
 
-function fadeinimg(imageId) {
-document.getElementById(imageId).style.opacity = '1';
+function fadeinimg(elementId) {
+document.getElementById(elementId).style.opacity = '1';
 }
 
 function fadeoutimg(imageId) {
 document.getElementById(imageId).style.opacity = '0';
 }
 
-function reheight(imageId) {
-document.getElementById(imageId).style.position = 'fixed';
-document.getElementById(imageId).style.height = 'auto';
-}
 
 
 
@@ -28,3 +24,21 @@ showImage('liylembed');
 function liyl2(){
 fadeinimg('liylembed2');
 }
+
+
+//THIS FUNCTION TAKES THE ELEMENT ID AND FADES IT IN, TO USE WITH THE ONCLICK FUNCTION IN HTML
+//function display(elementId){
+//$(elementId).fadeIn(750);
+//}
+
+//SAME AS THE FUNCTION ABOVE, BUT DOES THE ONCLICK EVENT THROUGH JQUERY INSTEAD OF HTML, UTILIZING THE HTML ID TAG
+$('#liylgrid').click(function(){
+  $('#liylembed').fadeIn(750);
+  document.getElementById('reelembed').style.display = 'none';
+});
+
+//WE WANT AN ALL-PURPOSE JQUERY COMMAND THAT TAKES IN THE CLICKED ELEMENT AND FADES IN THE CORRESPONDING EMBED
+$('#reelgrid').click(function(){
+  $('#reelembed').fadeIn(750);
+  document.getElementById('liylembed').style.display = 'none';
+});
