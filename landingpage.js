169acb77ed20b97685f84f37eeb3d53fe85692eb
Raosk <sampreeth20@Hotmail.com>
2016-01-07 13:21:17 -0500

    Landing Page Creation
    Created the basis of the landing page, with scalable image maps and
    randomizing image on refresh. Still need to add approx. 8 images and
    corresponding image maps.

---
+++ landingpage.js
@@ -0,0 +1,26 @@
+
+
+
+var numberofimages = 2;
+var randomnumber = Math.floor(numberofimages*Math.random());
+$('#' + randomnumber + 'bg').show();
+
+
+
+//$('#' + randomnumber + 'bg').css('display','block');
+//$('#' + randomnumber + 'bg').attr('id','bg');
+
+
+
+/*var image = new Array ();
+image[0] = "img5crop.jpg";
+image[1] = "img1crop.jpg";
+image[2] = "http://placehold.it/40";
+image[3] = "http://placehold.it/50";
+var size = image.length
+var x = Math.floor(size*Math.random())
+
+$('#random').attr('src',image[x]);
+$('#random').attr('id','bgvert');
+$('#random').attr('usemap','#girlmap');*/
+//document.getElementById("random").useMap = "#girlmap";
