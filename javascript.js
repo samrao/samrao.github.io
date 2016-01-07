01d6e2da2fbd36a84f3c20cfcba386c4c47243f5
Raosk <sampreeth20@Hotmail.com>
2016-01-06 20:17:06 -0500

    Website Updates
    More javascript updates, adding in embed descriptions

--- javascript.js
+++ javascript.js
@@ -1,60 +1,55 @@
-//Hide bw images on pageload
+//HIDE BW IMAGES AND EMBEDS ON PAGELOAD bw images on pageload
 $(document).ready(function(){
-    $("[id$='bw']").css('display','none');
-    $("[id$='embed']").css('display','none');
+    $("[id$='bw']").hide();
+    $("[id$='embed']").hide();
+    $("[id$='embedtext']").hide();
 });
 
-//select all elements with an id that ends with "grid", and assign click behaviour
+
+//SELECT ALL ELEMENTS WITH AN ID THAT ENDS WITH "GRID", AND ASSIGN CLICK BEHAVIOUR
 $("[id$='grid']").click(function(){
-  //select all elements with an id that ends with "embed", and hide them
-  $("[id$='embed']").hide();
-  //create a variable that stores the id of the clicked link
-  var clicked = this.id;
-  //at this point, the variable "clicked" ends with "grid", so replace with "embed"
-  clicked = clicked.replace('grid','embed');
-  //fade in the embed corresponding to the clicked element, which has an id of the form "____embed"
-  $("#" + clicked).fadeIn(750);
-  //alert (clicked);
+  //MAKE EMBEDS FADE IN ON CLICK OF CORRESPONDING GRID ELEMENT
+    //select all elements with an id that ends with "embed", and hide them
+    $("[id$='embed']").hide();
+    $("[id$='embedtext']").hide();
+    //create a variable that stores the id of the clicked link
+    var clicked = this.id;
+    //at this point, the variable "clicked" ends with "grid", so replace with "embed"
+    clicked = clicked.replace('grid','embed');
+    //fade in the embed corresponding to the clicked element, which has an id of the form "____embed"
+    $("#" + clicked).fadeIn(750);
+    $("#" + clicked + "text").fadeIn(900);
+    //alert (clicked);
 
 
-  //remain bold on click, make everything else in the grid have a font style of "lighter"
-  var clickedtext = this.id;
-  clickedtext = clickedtext.replace('grid','text');
-  $("[id$='text']").css("font-weight","lighter");
-  $("#" + clickedtext).css("font-weight","Bold");
-
-
-  //replace all black and white images with the corresponding coloured pair
-  $("[id$='bw']").hide();
-  $("[id$='img']").show();
-  var clickedimg = this.id
-  clickedimg = clickedimg.replace('grid','img');
-  //on click, hide the coloured image and show the bw image
-  $("#" + clickedimg).hide();
-  $("#" + clickedimg + "bw").show();
-
-
-  //make the text in the grid bold when mouseover corresponding grid element
-  $("[id$='grid']").mouseover(function(){
-    var hoveredtext = this.id
-    hoveredtext = hoveredtext.replace('grid','text');
-    $("#" + hoveredtext).css("font-weight","Bold");
-  });
-  $("[id$='grid']").mouseout(function(){
-    var hoveredtext = this.id
-    hoveredtext = hoveredtext.replace('grid','text');
-    //change the selector to not include the text corresponding to the clicked grid element
-    $("#" + hoveredtext).css("font-weight","lighter");
-    //makes all text in grid lighter, except the one that's clicked
-    $("[id$='text']").css("font-weight","lighter");
+  //TEXT REMAINS BOLD ON CLICK, EVERYTHING ELSE HAS A FONT STYLE OF "LIGHTER"
+    var clickedtext = this.id;
+    clickedtext = clickedtext.replace('grid','gridtext');
+    $("[id$='gridtext']").css("font-weight","lighter");
     $("#" + clickedtext).css("font-weight","Bold");
-  });
 
+  //MAKE IMAGES STAY BW ON CLICK
+    //replace all black and white images with the corresponding coloured pair
+    $("[id$='bw']").hide();
+    $("[id$='img']").show();
+    var clickedimg = this.id
+    clickedimg = clickedimg.replace('grid','img');
+    //on click, hide the coloured image and show the bw image
+    $("#" + clickedimg).hide();
+    $("#" + clickedimg + "bw").show();
+
+
+  //MAKE TEXT IN THE GRID BOLD WHEN MOUSEOVER CORRESPONDING ELEMENT
+    $("[id$='grid']").mouseover(function(){
+      var hoveredtext = this.id
+      hoveredtext = hoveredtext.replace('grid','gridtext');
+      $("#" + hoveredtext).css("font-weight","Bold");
+    });
+    $("[id$='grid']").mouseout(function(){
+      //makes all text in grid lighter, except the one that's clicked
+      $("[id$='gridtext']").css("font-weight","lighter");
+      $("#" + clickedtext).css("font-weight","Bold");
+    });
+
+  //create a function that makes video stop playing when another link is clicked
 });
-
-
-//I WANT AN ALL-PURPOSE JQUERY COMMAND THAT TAKES IN THE CLICKED ELEMENT AND FADES IN THE CORRESPONDING EMBED
-/*$('#____grid').click(function(){ //when you click on a ____grid link
-  $('#____embed').fadeIn(750); //you fade in ____embed
-  document.getElementById('____embed').style.display = 'none'; //all other ____embed not equal to the clicked ____grid disappear
-});*/
