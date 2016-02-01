d35c69163488181eaee24fb7083dd6cef07f4cd0
Raosk <sampreeth20@Hotmail.com>
2016-01-31 20:15:27 -0500

    Changing contact form email

--- contactform.js
+++ contactform.js
@@ -1,2 +1,2 @@
     var contactform =  document.getElementById('contactform');
-    contactform.setAttribute('action', '//formspree.io/' + 'your' + '@' + 'email' + '.' + 'com');
+    contactform.setAttribute('action', '//formspree.io/' + 'sampreeth20' + '@' + 'hotmail' + '.' + 'com');
