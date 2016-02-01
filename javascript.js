d464e3bc2938493f459d44bc1c1afffff8b90100
Raosk <sampreeth20@Hotmail.com>
2016-01-31 19:08:32 -0500

    More contact form trials

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+//CONTACT FORM STUFF
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
+
+
+
+
 //HIDE BW IMAGES AND EMBEDS ON PAGELOAD
   $(document).ready(function(){
       $("[id$='bw']").hide();
